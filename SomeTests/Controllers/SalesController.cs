eadca3aff4e8ab9baa5da6e6e175ad729e72bc8e
Ihateshestakov <baranka2003@yandex.ru>
2022-05-22 20:52:36 +0300

    Добавьте файлы проекта.

---
+++ SomeTests/Controllers/SalesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SomeTests.Models;
+using SomeTests.Services;
+
+namespace SomeTests.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly SaleService saleService;
+
+        public SalesController(SaleService saleService)
+        {
+            this.saleService = saleService;
+        }
+
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(saleService.Get());
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = saleService.Get(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            return View(sale);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                saleService.Create(sale);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sale);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = saleService.Get(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Cars/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, Sale sale)
+        {
+            if (id != sale.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                saleService.Update(id, sale);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(sale);
+            }
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = saleService.Get(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            try
+            {
+                var sale = saleService.Get(id);
+
+                if (sale == null)
+                {
+                    return NotFound();
+                }
+
+                saleService.Remove(sale.Id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SomeTests/Services/SaleService.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Driver;
+using SomeTests.Models;
+
+namespace SomeTests.Services
+{
+    public class SaleService
+    {
+        private readonly IMongoCollection<Sale> sales;
+
+        public SaleService(IConfiguration config)
+        {
+            MongoClient client = new MongoClient(config.GetConnectionString("Nastya"));
+            IMongoDatabase database = client.GetDatabase("Nastya");
+            sales = database.GetCollection<Sale>("Sales");
+        }
+
+        public List<Sale> Get()
+        {
+            return sales.Find(sale => true).ToList();
+        }
+
+        public Sale Get(string id)
+        {
+            return sales.Find(sale => sale.Id == id).FirstOrDefault();
+        }
+
+        public Sale Create(Sale sale)
+        {
+            sales.InsertOne(sale);
+            return sale;
+        }
+
+        public void Update(string id, Sale saleIn)
+        {
+            sales.ReplaceOne(sale => sale.Id == id, saleIn);
+        }
+
+        public void Remove(Sale saleIn)
+        {
+            sales.DeleteOne(sale => sale.Id == saleIn.Id);
+        }
+
+        public void Remove(string id)
+        {
+            sales.DeleteOne(sale => sale.Id == id);
+        }
+    }
+}
